b9b3e0a0a6930d3a5029e6c10ade2287c9888d9f
dalia-sharra <Dalia-Sharra.Deiac@msg.group>
2022-07-29 15:08:37 +0300

    back

--- shop/src/main/resources/db/migration/V1_3__create_revenue_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS Revenue (
-
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    location varchar(150),
-    date DATE,
-    sum DECIMAL
-);
--- shop/src/main/resources/db/migration/V1_9__add_objects.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO ProductCategory (name, description)
-VALUES ('ProductCategory1', 'Description1');
-
-INSERT INTO Supplier (name)
-VALUES ('Supplier1');
